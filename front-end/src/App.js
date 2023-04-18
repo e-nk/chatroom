a1ee93de5353b80737d8b4ddae082104d0028e8f
e-nk <enocklagat747@gmail.com>
2023-04-18 13:03:15 +0300

    Components :home :dashboard

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter,Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      
+      <Switch>
+        <Route exac path={"/"}><Home/></Route>
+        <Route exac path={"/dashboard"} ><Dashboard/></Route>
+      </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+//   return (
+//     <div>
+//       {isLoggedOut ? <NavbarFront /> : <Navbar />}
+
+//       <Switch>
+//         <Route exact path="/products">
+//           <Products />
+//         </Route>
+//         <Route path="/new-arrivals">
+//           <NewArrivals />
+//         </Route>
+//         <Route path="/cart">
+//           <Cart />
+//         </Route>
+//         <Route path="/wishlist">
+//           <Wishlist />
+//         </Route>
+//         <Route path="/product/:productId">
+//           <ProductDetail />
+//         </Route>
+//         <Route path="/logout">
+//           <Logout onLogout={handleLogout} />
+//         </Route>
+//         <Route path="/home">
+//           <Home/>
+//         </Route>
+//         <Route path="/about">
+//           <About/>
+//         </Route>
+//         <Route path="/login">
+//           <Login/>
+//         </Route>
+//         <Route path="/reset-pass">
+//           <ForgotPassword/>
+//         </Route>
+//         <Route path="/orders">
+//           <Orders/>
+//         </Route>
+//         <Route path="/signup">
+//           <Signup/>
+//         </Route>
+//         <Route path="/forgot-password">
+//           <ForgotPassword/>
+//         </Route>
+
+//       </Switch>
+
+//       <Footer />
+//     </div>
+//   );
+// }
+
+
+// export default App;
