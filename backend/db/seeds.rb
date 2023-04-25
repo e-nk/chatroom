50a1dbb68d9b639c76f7965cb2fcc84ab4f95579
e-nk <enocklagat747@gmail.com>
2023-04-25 13:11:38 +0300

    admin updated

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,7 +2,7 @@ puts "seeding ..."
 
 User.create!(
     username: "admin",
-    email: "admin@mail.com",
+    email: "admin@gmail.com",
     password: "admins",
     password_confirmation: "admins",
     admin: true
